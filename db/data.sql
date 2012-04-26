221be0237499b96458a36abac65195bc779dab94
nazarov.andrey <nazarov.andrey@me.com>
2012-04-26 07:49:35 +0000

    minor fixes

--- db/data.sql
+++ db/data.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.62, for apple-darwin10.3.0 (i386)
+-- MySQL dump 10.13  Distrib 5.1.61, for debian-linux-gnu (i486)
 --
--- Host: localhost    Database: crm
+-- Host: localhost    Database: italy
 -- ------------------------------------------------------
--- Server version	5.1.62
+-- Server version	5.1.61-0ubuntu0.10.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,13 +16,41 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Dumping data for table `application`
+--
+
+LOCK TABLES `application` WRITE;
+/*!40000 ALTER TABLE `application` DISABLE KEYS */;
+/*!40000 ALTER TABLE `application` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `attachment`
+--
+
+LOCK TABLES `attachment` WRITE;
+/*!40000 ALTER TABLE `attachment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `attachment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Dumping data for table `attachment_type`
 --
 
 LOCK TABLES `attachment_type` WRITE;
 /*!40000 ALTER TABLE `attachment_type` DISABLE KEYS */;
-INSERT INTO `attachment_type` VALUES (1,'application'),(2,'organization'),(3,'person');
+INSERT INTO `attachment_type` (`id`, `code`) VALUES (1,'application'),(2,'organization'),(3,'person');
 /*!40000 ALTER TABLE `attachment_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `contact`
+--
+
+LOCK TABLES `contact` WRITE;
+/*!40000 ALTER TABLE `contact` DISABLE KEYS */;
+INSERT INTO `contact` (`id`, `person`, `type`, `value`) VALUES (1,1,3,'a.uezdin@cersanit.ru'),(2,1,2,'+79169002514'),(3,1,2,'+7 495 7470114'),(4,1,1,'alex.uezdin');
+/*!40000 ALTER TABLE `contact` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -31,8 +59,27 @@ UNLOCK TABLES;
 
 LOCK TABLES `contact_type` WRITE;
 /*!40000 ALTER TABLE `contact_type` DISABLE KEYS */;
-INSERT INTO `contact_type` VALUES (1,'skype'),(2,'phone'),(3,'mail');
+INSERT INTO `contact_type` (`id`, `code`) VALUES (1,'skype'),(2,'phone'),(3,'mail');
 /*!40000 ALTER TABLE `contact_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `legal_entity`
+--
+
+LOCK TABLES `legal_entity` WRITE;
+/*!40000 ALTER TABLE `legal_entity` DISABLE KEYS */;
+/*!40000 ALTER TABLE `legal_entity` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `organization`
+--
+
+LOCK TABLES `organization` WRITE;
+/*!40000 ALTER TABLE `organization` DISABLE KEYS */;
+INSERT INTO `organization` (`id`, `type`, `name`, `phone`, `site`, `address`, `country`) VALUES (1,2,'CERSANIT-FRIANOVO FACTORY','+7 495 747 01 14','www.cersanit.ru','via Molodejnaia, 15, pos. Fryanovo, Scelkovskiy raion, oblast di Mosca','Russia'),(2,2,'CERSANIT-KUCINO FACTORY','+7 495 9749763	','www.cersanit.ru','Kucino','Russia');
+/*!40000 ALTER TABLE `organization` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -41,8 +88,37 @@ UNLOCK TABLES;
 
 LOCK TABLES `organization_type` WRITE;
 /*!40000 ALTER TABLE `organization_type` DISABLE KEYS */;
-INSERT INTO `organization_type` VALUES (1,'supplier'),(2,'client');
+INSERT INTO `organization_type` (`id`, `code`) VALUES (1,'supplier'),(2,'client');
 /*!40000 ALTER TABLE `organization_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `person`
+--
+
+LOCK TABLES `person` WRITE;
+/*!40000 ALTER TABLE `person` DISABLE KEYS */;
+INSERT INTO `person` (`id`, `organization`, `name`, `position`, `comment`) VALUES (1,1,'Uezdin Alexey','Purchase Manager','');
+/*!40000 ALTER TABLE `person` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `report`
+--
+
+LOCK TABLES `report` WRITE;
+/*!40000 ALTER TABLE `report` DISABLE KEYS */;
+/*!40000 ALTER TABLE `report` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`, `login`, `pass`, `super`) VALUES (1,'admin','d033e22ae348aeb5660fc2140aec35850c4da997',0);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -54,4 +130,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-04-25 17:36:03
+-- Dump completed on 2012-04-26  7:14:46
---
+++ db/predef_data.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 5.1.61, for debian-linux-gnu (i486)
+--
+-- Host: localhost    Database: italy
+-- ------------------------------------------------------
+-- Server version	5.1.61-0ubuntu0.10.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `attachment_type`
+--
+
+LOCK TABLES `attachment_type` WRITE;
+/*!40000 ALTER TABLE `attachment_type` DISABLE KEYS */;
+INSERT INTO `attachment_type` VALUES (1,'application'),(2,'organization'),(3,'person');
+/*!40000 ALTER TABLE `attachment_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `contact_type`
+--
+
+LOCK TABLES `contact_type` WRITE;
+/*!40000 ALTER TABLE `contact_type` DISABLE KEYS */;
+INSERT INTO `contact_type` VALUES (1,'skype'),(2,'phone'),(3,'mail');
+/*!40000 ALTER TABLE `contact_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `organization_type`
+--
+
+LOCK TABLES `organization_type` WRITE;
+/*!40000 ALTER TABLE `organization_type` DISABLE KEYS */;
+INSERT INTO `organization_type` VALUES (1,'supplier'),(2,'client');
+/*!40000 ALTER TABLE `organization_type` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-04-26  7:10:48
