a351fcf36ed90545c462118af6854a118c36fcd5
nazarov.andrey <nazarov.andrey@me.com>
2012-05-02 00:01:03 +0400

    follow-up

--- private/ru/nazarov/crm/forms/AppForm.php
+++ private/ru/nazarov/crm/forms/AppForm.php
@@ -6,7 +6,7 @@
 			$this->addFieldExt('client', 'Client', Form::FIELD_SELECT, true)
 				->addFieldExt('supplier', 'Supplier', Form::FIELD_SELECT, true)
 				->addFieldExt('date', 'Date', null, true)
-			        ->addValidator(new \ru\nazarov\sitebase\core\Forms\RegexpValidator('[\d]{2}/[\d]{2}/[\d]{4}'), 'Invalid date Format')
+			        ->addValidator(new \ru\nazarov\sitebase\core\forms\RegexpValidator('[\d]{2}/[\d]{2}/[\d]{4}'), 'Invalid date Format')
 				->addFieldExt('comment', 'Comment', Form::FIELD_TEXTAREA)
 				->addFieldExt('notify_client', 'Notify client', Form::FIELD_INPUT_CHECKBOX)
 				->addFieldExt('attachment', '', Form::FIELD_NOT_RENDER)
