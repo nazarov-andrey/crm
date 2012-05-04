df28220d4fc876e96ef089fd682bb5b246798a40
nazarov.andrey <nazarov.andrey@me.com>
2012-05-04 11:52:16 +0400

    db scheme up

--- db/scheme.sql
+++ db/scheme.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.61, for debian-linux-gnu (i486)
+-- MySQL dump 10.13  Distrib 5.5.9, for osx10.6 (i386)
 --
 -- Host: localhost    Database: italy
 -- ------------------------------------------------------
--- Server version	5.1.61-0ubuntu0.10.04.1
+-- Server version	5.5.9
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -105,6 +105,7 @@ DROP TABLE IF EXISTS `legal_entity`;
 CREATE TABLE `legal_entity` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
+  `res_basename` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -223,4 +224,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-05-01 19:42:19
+-- Dump completed on 2012-05-04 11:52:12
